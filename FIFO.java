e00f1abcc08c02c53ea4a556e5fe18db01af57a6
Isak Wahlberg <isawah-6@A9737.its.ltu.se>
2017-03-07 16:44:10 +0100

    Klar

--- FIFO.java
+++ FIFO.java
@@ -1,11 +1,23 @@
 import java.util.NoSuchElementException;
 import java.util.ArrayList;
 
+/**
+ * This class is an implementation of First-In-First-Out queues.
+ * 
+ * @author isawah-6
+ *
+ */
+
 public class FIFO implements Queue{
 	
 	private int maxSize = 0;
 	private ArrayList<Object> FIFOQueue = new ArrayList<Object>();
 
+	/**
+	 * Adds arg0 to FIFO queue
+	 * 
+	 * @param arg0
+	 */
 	public void add(Object arg0) {
 		FIFOQueue.add(arg0);
 		if(FIFOQueue.size() >= maxSize) {
@@ -15,6 +27,11 @@ public class FIFO implements Queue{
 		}
 	}
 
+	/**
+	 * Returns the first object in the FIFO queue.
+	 * Throws NoSuchElementException is queue is empty.
+	 * @throws NoSuchElementException
+	 */
 	public Object first() throws NoSuchElementException {
 		if(FIFOQueue.size() == 0) {
 			throw new NoSuchElementException();
@@ -22,14 +39,25 @@ public class FIFO implements Queue{
 		return FIFOQueue.get(0);
 	}
 
+	/**
+	 * Returns true if FIFO queue is empty.
+	 */
 	public boolean isEmpty() {
 		return FIFOQueue.isEmpty();
 	}
 
+	/**
+	 * Returns the largest size that FIFO queue has had during its lifetime.
+	 */
 	public int maxSize() {
 		return maxSize;
 	}
 
+	/**
+	 * Removes the first element if FIFO queue and
+	 * if empty, throws NoSuchElementException.
+	 * @throws NoSuchElementException
+	 */
 	public void removeFirst() throws NoSuchElementException {
 		if(FIFOQueue.size() == 0) {
 			throw new NoSuchElementException();
@@ -37,34 +65,62 @@ public class FIFO implements Queue{
 		FIFOQueue.remove(0);
 	}
 
+	/**
+	 * Returns current size of FIFO queue.
+	 */
 	public int size() {
 		return FIFOQueue.size();
 	}
 	
+	/**
+	 * Returns FIFO queue.
+	 * @return FIFOQueue
+	 */
 	public ArrayList<Object> getQueue() {
 		return FIFOQueue;
 	}
 	
+	/**
+	 * Returns String that shows what the queue contains.
+	 */
 	public String toString() {
 		String message;
 		message = "Queue: ";
 		
 		for(int elem = 0; elem < FIFOQueue.size(); elem++) {
-			message += "(" + String.valueOf(elem) + ") ";
+			message += "(" + String.valueOf(this.FIFOQueue.get(elem)) + ") ";
 		}
 		
 		return message;
 	}
 	
+	/**
+	 * Checks if "this" and "f" are the same. If "f" is not of type FIFO, throws ClassCastException.
+	 * @param f
+	 * @throws ClassCastException
+	 */
 	public boolean equals(Object f) throws ClassCastException {
 		if(f instanceof FIFO == false) {
 			throw new ClassCastException();
 			
-		} else if(f instanceof FIFO && ((FIFO) f).size() == this.size()) {
-			for(int i = 0; i < ((FIFO) f).size(); i++) {
-				if(((FIFO) f).getQueue().get(i) != FIFOQueue.get(i)) {
-					return false;
+		} else {
+			if(((FIFO) f).size() == this.size()) {
+				for(int i = 0; i < ((FIFO) f).size(); i++) {
+					if(((FIFO) f).getQueue().get(i) == null && FIFOQueue.get(i) == null) {
+						continue;
+					}
+					if(FIFOQueue.get(i) == null && ((FIFO) f).getQueue().get(i) != null) {
+						return false;
+					}
+					if(FIFOQueue.get(i) != null && ((FIFO) f).getQueue().get(i) == null) {
+						return false;
+					}
+					if(((FIFO) f).getQueue().get(i) == FIFOQueue.get(i)) {
+						return false;
+					}
 				}
+			}else {
+				return false;
 			}
 		}
 		return true;
--- GraphIO.java
+++ GraphIO.java
@@ -1,31 +1,38 @@
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.Scanner;
 
-public class GraphIO {
+
+/**
+ * Reads file containing information of a graph and then adds the nodes and edges.
+ * @author isawah-6
+ *
+ */
+public class GraphIO {	
 	private static ArrayList<int[]> input = new ArrayList<int[]>();
 	private static int numberOfNodes;
 	
-	private static BufferedReader br = null;
-	
-	static public void readFile(Graph g, String filename) throws IOException {
+	/**
+	 * Reads from file "filename" and then adds the nodes and edges.
+	 * @param g
+	 * @param filename
+	 * @throws IOException
+	 */
+	static public void readFile(Graph g, String filename) throws IOException {	
 		
+		try {
+			Scanner scanner = new Scanner(new FileReader(filename));
+			input.add(new int[]{scanner.nextInt()});		
+			numberOfNodes = input.get(0)[0];
 		
-		numberOfNodes = input.get(0)[0];
-		
-		//File file = new File(filename);
-		Scanner scanner = new Scanner(new FileReader(filename));
-		
-		if(scanner.hasNextLine()) {
-			input.add(new int[]{scanner.nextInt()});
-		}
-		
-		while(scanner.hasNextLine()) {
-			input.add(new int[]{scanner.nextInt(), scanner.nextInt(), scanner.nextInt()});
-			break;
+			while(scanner.hasNext()) {
+				input.add(new int[]{scanner.nextInt(), scanner.nextInt(), scanner.nextInt()});
+			}
+			
+			scanner.close();
+		}catch (IOException e) {
+			throw e;
 		}
 		
 		addNodes(g);
@@ -39,7 +46,7 @@ public class GraphIO {
 	}
 	
 	private static void addEdges(Graph g) {
-		for(int i = numberOfNodes + 1; i <= input.size(); i++) {
+		for(int i = numberOfNodes + 1; i < input.size(); i++) {
 			g.addEdge(input.get(i)[0], input.get(i)[1], input.get(i)[2]);
 		}
 	}
